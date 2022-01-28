23e289c37d37c9109875a0eb99a5c7c757192094
Lakshman Mulchandani <74480329+lakshmanmulchandani@users.noreply.github.com>
2022-01-28 23:43:14 +0530

    Revert "sidebar added"
    This reverts commit 77cdfcac2812af853322cd3d028017b50ac0396d.

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,13 @@
 //Navbar
 import Navbar from "../components/Navbar.js";
-import "../styles/globals.css";
-import {ChakraProvider} from "@chakra-ui/react";
 
 import "../styles/globals.css";
 
-function MyApp({Component, pageProps}) {
+function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider>
+    <Navbar>
       <Component {...pageProps} />
-    </ChakraProvider>
+    </Navbar>
   );
 }
 
