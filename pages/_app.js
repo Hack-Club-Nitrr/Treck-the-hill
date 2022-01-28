a1cf85152f44ec474540f078e831b11b3b3dbf04
Shubham Shrikant Bawner <wurtzreaxnwittigreaxn@gmail.com>
2022-01-28 15:51:43 +0530

    v2

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,14 @@
 //Navbar
 import Navbar from "../components/Navbar.js";
+import Sponsers from "./sponsers.js";
 
 import "../styles/globals.css";
 
+
 function MyApp({ Component, pageProps }) {
   return (
-    <Navbar>
-      <Component {...pageProps} />
-    </Navbar>
+
+    <Sponsers/>
   );
 }
 
