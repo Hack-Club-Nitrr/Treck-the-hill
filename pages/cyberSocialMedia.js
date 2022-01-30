7b916f6c4405762ae64cce157a9f1d30c563465e
Alim Khan <90334104+Mh-Alim@users.noreply.github.com>
2022-01-30 16:08:12 +0530

    basic css added in hackclub and cyber junk social media

--- pages/cyberSocialMedia.js
+++ pages/cyberSocialMedia.js
@@ -20,12 +20,18 @@ const cyberSocialMedia = () => {
         .social-media-text{
           width: fit-content;
           margin: 20px auto;
+          margin-bottom: 30px;
+          color: white;
           font-size: 1.6rem;
           border-top: 2px solid black;
-          border-bottom: 2px solid;
+          border-bottom: 2px solid black;
         }
         .container{
           padding: 20px;
+          background: #0052D4;  /* fallback for old browsers */
+            background: -webkit-linear-gradient(to right, #6FB1FC, #4364F7, #0052D4);  /* Chrome 10-25, Safari 5.1-6 */
+            background: linear-gradient(to right, #6FB1FC, #4364F7, #0052D4); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+
         }
         `}</style>
     </div>
