7276c0f22d87969fba3cca4d40c3117f14cdbfde
Alim Khan <90334104+Mh-Alim@users.noreply.github.com>
2022-01-30 14:31:12 +0530

    Hack club social media handle added

---
+++ pages/socialMedia.js
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import styles from "../styles/Home.module.css";
+import Head from 'next/head';
+const socialMedia = () => {
+  return (
+    <div> 
+    <Head>
+      <title>social-media</title>
+      
+      <script type="text/javascript">{`(function(){ var i,e,d=document,s="script";i=d.createElement("script");i.async=1;i.charset="UTF-8"; i.src="https://cdn.curator.io/published/01b5ffb5-a217-455b-986d-ab9195b624c4.js"; e=d.getElementsByTagName(s)[0];e.parentNode.insertBefore(i, e); })();`}</script>
+    </Head>
+    <div><h1>Hack Club Instagram Handle</h1></div>
+    <br />
+    <br />
+    <div id="curator-feed-default-feed-layout"><a href="https://curator.io" target="_blank" className="crt-logo crt-tag">Powered by Curator.io</a></div>
+    </div>
+  )
+  
+};
+
+export default socialMedia;
