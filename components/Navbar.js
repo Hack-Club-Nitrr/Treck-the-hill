137649775a78bee4b2d13de8752bd3188e2ac242
ADITYA-RAY <74045024+ADITYA-RAY@users.noreply.github.com>
2022-02-02 19:37:19 +0530

    console errors removed

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,14 +1,14 @@
-import Sidebar from "./Sidebar";
-import { Flex, Text, IconButton } from "@chakra-ui/react";
-import { FiMenu } from "react-icons/fi";
+// import Sidebar from "./Sidebar";
+// import { Flex, Text, IconButton } from "@chakra-ui/react";
+// import { FiMenu } from "react-icons/fi";
 
-const Navbar = ({ children }) => {
-  return (
-    <div style={{ display: "grid", gridTemplateColumns: "1fr 9fr" }}>
-        <Sidebar />
-      {children}
-    </div>
-  );
-};
+// const Navbar = ({ children }) => {
+//   return (
+//     <div style={{ display: "grid", gridTemplateColumns: "1fr 9fr" }}>
+//         <Sidebar />
+//       {children}
+//     </div>
+//   );
+// };
 
-export default Navbar;
+// export default Navbar;
