7a27083db4a0f8ddc05c91dad52a9ae18b3cc7fb
Pramil Kesarwani <Kesarwanipramil01@gmail.com>
2022-02-03 14:32:04 +0530

    Changes in timeline made

--- pages/timeline.js
+++ pages/timeline.js
@@ -1,27 +1,18 @@
 import React from 'react';
-import Fade from 'react-reveal/Fade';
-
+import styles from '../styles/timeline.module.css';
 import { timeLineData } from '../components/Timeline/timeLineData';
 import TimeLineItem from '../components/Timeline/TimeLineItem';
 const timeline = () => {
   return (
-    <div className="timeline-page">
+    <div className={styles.timeline_page}>
       {/* <h1>hello</h1> */}
-      <div className="timeline-container">
-        <h1 className="main-heading">Event Timeline</h1>
+      <div className={styles.timeline_container}>
+        <h1 className={styles.main_heading}>Event Timeline</h1>
         {timeLineData.map((data, index) => {
           if (index % 2 === 0) {
-            return (
-              <Fade>
-                <TimeLineItem key={index} data={data} id={index} />
-              </Fade>
-            );
+            return <TimeLineItem key={index} data={data} id={index} />;
           } else {
-            return (
-              <Fade>
-                <TimeLineItem key={index} data={data} id={index} />
-              </Fade>
-            );
+            return <TimeLineItem key={index} data={data} id={index} />;
           }
         })}
       </div>
