30e6d19207d443286c6ba2d46b8f83616812d3c2
ADITYA-RAY <74045024+ADITYA-RAY@users.noreply.github.com>
2022-02-03 20:29:49 +0530

    timeline css updated

---
+++ components/Sponsers.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { useEffect, useState, useRef } from 'react';
+import axios from 'axios';
+
+export const Sponsers = () => {
+  const [sponsers, setSponsers] = useState([]);
+  const [diamond, setdiamond] = useState([]);
+  const [silver, setSilver] = useState([]);
+  const [gold, setGold] = useState([]);
+  const [platinum, setPlatinum] = useState([]);
+  const [bronze, setBronze] = useState([]);
+
+  useEffect(() => {
+    const cat1 = axios
+      .get('https://trekthehill.herokuapp.com/api/sponser/')
+      .then((response) => {
+        console.log(response);
+        setSponsers(response.data);
+
+        // for(var i=0; i< sponsers.length;i++){
+        //     if(sponsers[i].category==="diamond"){
+        //         setdiamond()
+        //     }
+        // }
+      })
+      .catch((error) => {});
+  }, []);
+
+  return (
+    <div>
+      {/* <div className="specialSponsers" style={bgStyle}>
+        <div className="platinum">{platinumSponsers}</div>
+        <div className="gold">{goldSponsers}</div>
+        <div className="silver">{silverSponsers}</div>
+        <div className="general">{generalSponsers}</div>
+      </div> */}
+    </div>
+  );
+};
