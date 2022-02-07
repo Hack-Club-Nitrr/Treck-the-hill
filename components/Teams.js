54f2e7e83b5ac7cc43cecabf5c787841b3bc20f1
Pramil Kesarwani <Kesarwanipramil01@gmail.com>
2022-02-07 13:23:08 +0530

    teams separated by domains

--- components/Teams.js
+++ components/Teams.js
@@ -4,45 +4,79 @@ import axios from 'axios';
 import styles from '../styles/teams.module.css';
 
 const Teams = () => {
-  const [toggler, setToggler] = useState(true);
   const [teamsData, setTeamsData] = useState([]);
+  const [displayedTeam, setDisplayedTeam] = useState([]);
 
   useEffect(() => {
     axios
       .get('https://trekthehill.herokuapp.com/api/team/')
       .then((res) => setTeamsData(res.data))
-      .catch((err) => console.log(err));
+      .catch((err) => console.log(err.response));
+    setDisplayedTeam(teamsData.filter((member) => member.domain === 'lead'));
   }, []);
+  const handleClick = (e) => {
+    setDisplayedTeam(
+      teamsData.filter((member) => member.domain === e.target.name)
+    );
+  };
 
   return (
     <div className={styles.team}>
       <h1 className={styles.teamInt}>Meet Our Team</h1>
       <br />
-      <div className={styles.toggleButton}>
-        <button
-          onClick={() => setToggler(!toggler)}
-          className={`${toggler && styles.buttonBorder} ${styles.btn}`}
-        >
-          HACK CLUB
-        </button>
-        <button
-          onClick={() => setToggler(!toggler)}
-          className={`${!toggler && styles.buttonBorder} ${styles.btn}`}
-        >
-          CYBER JUNK
-        </button>
-      </div>
-      {toggler && (
-        <div className={styles.A}>
-          {teamsData.map((member, ind) => {
-            return <TeamCard delay={500} member={member} key={ind} />;
-          })}
+      {displayedTeam.length !== 0 && (
+        <div className={styles.toggleButton}>
+          <button
+            onClick={handleClick}
+            name="lead"
+            className={`${
+              displayedTeam[0].domain === 'lead' && styles.buttonBorder
+            } ${styles.btn}`}
+          >
+            LEAD
+          </button>
+          <button
+            onClick={handleClick}
+            name="web"
+            className={`${
+              displayedTeam[0].domain === 'web' && styles.buttonBorder
+            } ${styles.btn}`}
+          >
+            WEB
+          </button>
+          <button
+            onClick={handleClick}
+            name="design"
+            className={`${
+              displayedTeam[0].domain === 'design' && styles.buttonBorder
+            } ${styles.btn}`}
+          >
+            DESIGN
+          </button>
+          <button
+            onClick={handleClick}
+            name="documentation"
+            className={`${
+              displayedTeam[0].domain === 'documentation' && styles.buttonBorder
+            } ${styles.btn}`}
+          >
+            DOCS
+          </button>
+          <button
+            onClick={handleClick}
+            name="sponsership"
+            className={`${
+              displayedTeam[0].domain === 'sponsership' && styles.buttonBorder
+            } ${styles.btn}`}
+          >
+            SPONSERSHIP
+          </button>
         </div>
       )}
-      {!toggler && (
-        <div className={styles.B}>
-          {teamsData.map((member, ind) => {
-            return <TeamCard delay={500} member={member} key={ind} />;
+      {displayedTeam.length !== 0 && (
+        <div className={styles.A}>
+          {displayedTeam.map((member, ind) => {
+            return <TeamCard member={member} key={ind} />;
           })}
         </div>
       )}
