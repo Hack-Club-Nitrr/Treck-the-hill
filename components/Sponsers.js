1fcb2850ffc1a3195b8ce6477247e16cd58e92b1
ADITYA-RAY <74045024+ADITYA-RAY@users.noreply.github.com>
2022-02-11 21:01:55 +0530

    sponsors name removed

--- components/Sponsers.js
+++ components/Sponsers.js
@@ -55,7 +55,7 @@ export const Sponsers = () => {
             <div className={styles.diamond} key={sponser.name}>
               <a href={sponser.link}>
                 <img src={sponser.image} alt={sponser.name} />
-                <div className={styles.spons_name}>{sponser.name}</div>
+                {/* <div className={styles.spons_name}>{sponser.name}</div> */}
               </a>
             </div>
           );
